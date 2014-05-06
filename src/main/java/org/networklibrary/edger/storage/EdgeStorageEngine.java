dd9bd4ea787c5a9dadbe91ede34945b1f4aa438f
Georg Summer <georg.summer@gmail.com>
2014-05-06 08:26:46 +0200

    added support for multiple hits. used by default no parameter config yet.

--- src/main/java/org/networklibrary/edger/storage/EdgeStorageEngine.java
+++ src/main/java/org/networklibrary/edger/storage/EdgeStorageEngine.java
@@ -1,8 +1,11 @@
 package org.networklibrary.edger.storage;
 
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 import java.util.logging.Logger;
 
 import org.neo4j.graphdb.DynamicRelationshipType;
@@ -16,43 +19,81 @@ import org.networklibrary.core.types.EdgeData;
 
 public class EdgeStorageEngine extends MultiTxStrategy<EdgeData> {
 	protected static final Logger log = Logger.getLogger(EdgeStorageEngine.class.getName());
-	
+
 	private final static String MATCH = "matchid";
-	private Map<String,Node> nodeCache = new HashMap<String,Node>();
+	private Map<String,Set<Node>> nodeCache = new HashMap<String,Set<Node>>();
 	
+	private int numEdges = 0;
+
 	public EdgeStorageEngine(GraphDatabaseService graph, ConfigManager confMgr) {
 		super(graph, confMgr);
 	}
 
 	@Override
 	protected void doStore(EdgeData curr) {
-		Node from = getNode(curr.getFromID(),getGraph());
-		Node to = getNode(curr.getToID(),getGraph());
+		Set<Node> froms = getNode(curr.getFromID(),getGraph());
+		Set<Node> tos = getNode(curr.getToID(),getGraph());
 
-		if(from == null || to == null){
+		if(froms == null || tos == null){
 			log.warning("failed to store edge: " + curr.toString());		
-			log.warning("from = " + from + " to = " + to);
+			log.warning("from = " + froms + " to = " + tos);
 			return;
 		}
 
-		Relationship r = from.createRelationshipTo(to, DynamicRelationshipType.withName(curr.getType()));
-		for(Entry<String,Object> prop : curr.getProperties().entrySet()) {
-			r.setProperty(prop.getKey(), prop.getValue());
+		for(Node from : froms){
+			for(Node to : tos){
+				Relationship r = from.createRelationshipTo(to, DynamicRelationshipType.withName(curr.getType()));
+				for(Entry<String,Object> prop : curr.getProperties().entrySet()) {
+					addProperty(prop.getKey(),prop.getValue(),r);
+				}
+				++numEdges;
+			}
 		}
 
 	}
 
-	protected Node getNode(String name, GraphDatabaseService g){
-		Node result = nodeCache.get(name);
+	private void addProperty(String key, Object prop, Relationship r) {
+		
+//		if(prop instanceof Collection<?>){
+//			Collection<?> collection = (Collection<?>)prop;
+//
+//			Object[] arr = ((Collection) prop).toArray();
+//
+//			if(arr[0] instanceof String){
+//				String[] values = (String[])arr;
+//				r.setProperty(key, values);
+//			}
+//		} else {
+			r.setProperty(key, prop);
+//		}
+	}
+	
+	@Override
+	public void finishUp() {
+		super.finishUp();
+		log.info("edges stored: " + numEdges);
+	}
+
+	protected Set<Node> getNode(String name, GraphDatabaseService g){
+		Set<Node> result = nodeCache.get(name);
 
 		if(result == null){
+			result = new HashSet<Node>();
+			
 			IndexHits<Node> hits = g.index().forNodes("matchable").get(MATCH, name);
 
 			if(hits.size() > 1){
-				log.warning("query for name = " + name + " returned more than one hit. Defaulting to first.");
+				log.warning("query for name = " + name + " returned more than one hit.");
+			}
+			
+			if(hits.size() == 0){
+				log.warning("could not find a hit for name = " + name);
 			}
 
-			result = hits.getSingle();
+			for(Node n : hits){
+				result.add(n);
+			}
+			hits.close();
 			nodeCache.put(name,result);
 		}
 
