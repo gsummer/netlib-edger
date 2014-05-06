177d58c19c87092474ec42f75493c9aa8ae46354
Georg Summer <georg.summer@gmail.com>
2014-05-06 08:28:00 +0200

    fixed miRTarbase import. pretty significant rewrite and adaptation. next time: do not expect network like output it downloads but db export.

--- src/main/java/org/networklibrary/edger/parsing/MirTarBaseParser.java
+++ src/main/java/org/networklibrary/edger/parsing/MirTarBaseParser.java
@@ -1,11 +1,14 @@
 package org.networklibrary.edger.parsing;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.networklibrary.core.parsing.FileBasedParser;
 import org.networklibrary.core.parsing.ParsingErrorException;
@@ -14,37 +17,89 @@ import org.networklibrary.core.types.EdgeData;
 public class MirTarBaseParser extends FileBasedParser<EdgeData> {
 	public final static String EDGE_TYPE = "miR_targeting";
 	public final static String SOURCE_NAME = "miRTarBase";
-	
+
+	private String carryOver = null;
+
 	private List<String> columns = null;
 
 	@Override
 	public Collection<EdgeData> parse() throws ParsingErrorException {
-		
-		String line = readLine();
+
 		List<EdgeData> res = null;
 		
-		if(line != null && !line.isEmpty()){
+		Map<String,Object> props = new HashMap<String,Object>();
+		props.put("data_source",SOURCE_NAME);
 		
-			res = new LinkedList<EdgeData>();
-			
-			String[] values = line.split("\\t",-1);
-			
-			String from = values[1];
-			
-			String to = values[4];
-			
-			Map<String,Object> props = new HashMap<String,Object>();
-			for(int i = 7; i < values.length; ++i){
-				if(!values[i].isEmpty()){
-					props.put(columns.get(i), Integer.valueOf(values[i]));
-				}
+		String from = null;
+		String to = null;
+		
+		String currMirId = null;
+		String lineMirId = null;
+
+		
+		ArrayList<String> experiments = new ArrayList<String>();
+		ArrayList<String> supportTypes = new ArrayList<String>();
+		ArrayList<String> publications = new ArrayList<String>();
+		
+		String line = null;
+		
+		do {
+			if(carryOver != null){
+				line = carryOver;
+				carryOver = null;
 			}
-			props.put("data_source",SOURCE_NAME);
-			
-			res.add(new EdgeData(from, to, EDGE_TYPE, props));
+			else {
+				line = readLine();
+			}
+	
+			if(line != null && !line.isEmpty()){
+				if(res == null){
+					res = new LinkedList<EdgeData>();
+				}
+
+				String[] values = line.split("\\t",-1);
+
+				lineMirId = values[0];
+				
+				if(currMirId == null){
+					currMirId = lineMirId;
+				}
+				
+				if(!currMirId.equals(lineMirId)){
+					break;
+				}
+				
+
+				from = values[1];
+				to = values[4];
+
+				experiments.add(values[6]);
+				supportTypes.add(values[7]);
+				publications.add(values[8]);
+				
+			}
+//			line = readLine();
+		} while(currMirId.equals(lineMirId) && line != null);
+
+		props.put(columns.get(6), experiments.toArray(new String[experiments.size()]));
+		props.put(columns.get(7), supportTypes.toArray(new String[supportTypes.size()]));
+		props.put(columns.get(8), publications.toArray(new String[publications.size()]));
+		props.put(columns.get(0), currMirId);
 		
+		res.add(new EdgeData(from, to, EDGE_TYPE, props));
+
+		if(line != null && !line.isEmpty()){
+			carryOver = line;
 		}
+		
 		return res;
+	}
+
+	protected void putProp(String key, String value,Map<String,Object> props){
+		if(!props.containsKey(key)){
+			props.put(key, new HashSet<String>());
+		}
+		((Set<String>)props.get(key)).add(value);
 	}
 
 	@Override
@@ -64,5 +119,10 @@ public class MirTarBaseParser extends FileBasedParser<EdgeData> {
 	@Override
 	protected void parseHeader(String header) {
 		columns = Arrays.asList(header.split("\\t",-1));
+	}
+	
+	@Override
+	public boolean ready() throws ParsingErrorException {
+		return super.ready() || carryOver != null;
 	}
 }
