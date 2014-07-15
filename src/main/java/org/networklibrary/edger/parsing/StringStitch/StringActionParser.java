d28abc2dbe6fbc8438a9e52ce9bb92cd562859ad
Georg Summer <georg.summer@gmail.com>
2014-07-15 13:20:51 +0200

    added a cutoff extra parameter to restrict the string edges based on their combined_score in links or score in actions

--- src/main/java/org/networklibrary/edger/parsing/StringStitch/StringActionParser.java
+++ src/main/java/org/networklibrary/edger/parsing/StringStitch/StringActionParser.java
@@ -6,59 +6,70 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Logger;
 
 import org.networklibrary.core.parsing.FileBasedParser;
 import org.networklibrary.core.parsing.ParsingErrorException;
 import org.networklibrary.core.types.EdgeData;
 
 public class StringActionParser extends FileBasedParser<EdgeData> {
+
+	protected static final Logger log = Logger.getLogger(StringLinkParser.class.getName());
+
 	public static String SOURCE_NAME = "string_actions";
-	
+
 	private List<String> columns = null;
-	
+
+	private int cutoff = 0;
+
 	public StringActionParser(){
 	}
-	
+
 	public Collection<EdgeData> parse() throws ParsingErrorException {
 		String line = readLine();
 		List<EdgeData> res = null;
-		
+
 		if(!line.isEmpty()){
 			res = new LinkedList<EdgeData>();
-			
+
 			String[] values = line.split("\\t",-1);
-			
+
 			if(values.length != columns.size()){
 				throw new IllegalArgumentException("number of elements in row does not match number of columns " + line);
 			}
-			
-//			item_id_a	item_id_b	mode	action	a_is_acting	score	sources	transferred_sources
-			
-			Map<String,Object> props = new HashMap<String,Object>();
-			
-			String oSource = values[7];
-			if(oSource.isEmpty() || oSource == null){
-				oSource = values[6];
+
+			//	item_id_a	item_id_b	mode	action	a_is_acting	score	sources	transferred_sources
+
+			int score = Integer.parseInt(values[5]);
+
+			if(score > cutoff){
+
+				Map<String,Object> props = new HashMap<String,Object>();
+
+				String oSource = values[7];
+				if(oSource.isEmpty() || oSource == null){
+					oSource = values[6];
+				}
+
+				if(!values[3].isEmpty()){
+					props.put("action", values[3]);
+				}
+
+				props.put("score", score);
+				props.put("orig_source", oSource);
+
+				props.put("data_source",SOURCE_NAME);
+				String from = values[0].replace("9606.","");
+				String to = values[1].replace("9606.","");
+
+				res.add(new EdgeData(from,to,values[2],props));
+				if("0".equals(values[4])){
+					res.add(new EdgeData(to,from,values[2],props));
+				}
 			}
-			
-			if(!values[3].isEmpty()){
-				props.put("action", values[3]);
-			}
-			
-			props.put("score", Integer.parseInt(values[5]));
-			props.put("orig_source", oSource);
-			
-			props.put("data_source",SOURCE_NAME);
-			String from = values[0].replace("9606.","");
-			String to = values[1].replace("9606.","");
-			
-			res.add(new EdgeData(from,to,values[2],props));
-			if("0".equals(values[4])){
-				res.add(new EdgeData(to,from,values[2],props));
-			}
-			
+
 		}
-		
+
 		return res;
 	}
 
@@ -72,11 +83,24 @@ public class StringActionParser extends FileBasedParser<EdgeData> {
 
 	@Override
 	public boolean hasExtraParameters() {
-		return false;
+		return true;
 	}
 
 	@Override
 	public void takeExtraParameters(List<String> extras) {
+		log.info("processing extra parameters: " + extras.toString());
+
+		for(String extra : extras){
+			String values[] = extra.split("=",-1);
+
+			switch(values[0]) {
+			case "cutoff":
+				cutoff = Integer.parseInt(values[1]);
+				break;
+			}
+		}
+
+		log.info("using a cutoff of " + cutoff);
 	}
 
 }
--- src/main/java/org/networklibrary/edger/parsing/StringStitch/StringLinkParser.java
+++ src/main/java/org/networklibrary/edger/parsing/StringStitch/StringLinkParser.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Logger;
 
 import org.networklibrary.core.parsing.FileBasedParser;
 import org.networklibrary.core.parsing.ParsingErrorException;
@@ -13,41 +14,48 @@ import org.networklibrary.core.types.EdgeData;
 
 public class StringLinkParser extends FileBasedParser<EdgeData> {
 
+	protected static final Logger log = Logger.getLogger(StringLinkParser.class.getName());
+
 	public static String EDGE_TYPE = "ppi";
 	public static String SOURCE_NAME = "string_links";
-	
+
 	private List<String> columns = null;
-	
+
+	protected int cutoff = 0;
+
 	public StringLinkParser(){
 	}
-	
+
 	public Collection<EdgeData> parse() throws ParsingErrorException {
 		String line = readLine();
 		List<EdgeData> res = null;
-		
+
 		if(!line.isEmpty()){
 			res = new LinkedList<EdgeData>();
-			
+
 			String[] values = line.split("\\s",-1);
-			
+
 			if(values.length != columns.size()){
 				throw new IllegalArgumentException("number of elements in row does not match number of columns " + line);
 			}
-			
-			Map<String,Object> props = new HashMap<String,Object>();
-			for(int i = 2; i < values.length; ++i){
-				if(!values[i].isEmpty()){
-					props.put(columns.get(i), Integer.valueOf(values[i]));
+
+			int combined_score = Integer.valueOf(values[values.length-1]);
+			if(combined_score > cutoff){
+				Map<String,Object> props = new HashMap<String,Object>();
+				for(int i = 2; i < values.length; ++i){
+					if(!values[i].isEmpty()){
+						props.put(columns.get(i), Integer.valueOf(values[i]));
+					}
 				}
+				props.put("data_source",SOURCE_NAME);
+				String from = values[0].replace("9606.","");
+				String to = values[1].replace("9606.","");
+
+				res.add(new EdgeData(from,to,EDGE_TYPE,props));
 			}
-			props.put("data_source",SOURCE_NAME);
-			String from = values[0].replace("9606.","");
-			String to = values[1].replace("9606.","");
-			
-			res.add(new EdgeData(from,to,EDGE_TYPE,props));
-			
+
 		}
-		
+
 		return res;
 	}
 
@@ -61,10 +69,25 @@ public class StringLinkParser extends FileBasedParser<EdgeData> {
 
 	@Override
 	public boolean hasExtraParameters() {
-		return false;
+		return true;
 	}
 
 	@Override
 	public void takeExtraParameters(List<String> extras) {
+
+		log.info("processing extra parameters: " + extras.toString());
+
+		for(String extra : extras){
+			String values[] = extra.split("=",-1);
+
+			switch(values[0]) {
+			case "cutoff":
+				cutoff = Integer.parseInt(values[1]);
+				break;
+			}
+		}
+
+		log.info("using a cutoff of " + cutoff);
+
 	}
 }
