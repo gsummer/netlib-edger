bf521b3032c69160d431834c85f7fe24e0588405
Thomas Kelder <thomas@edgeleap.com>
2015-02-05 16:22:36 +0100

    fixed pathvisio maven script to extract the jars within jars

--- add-pathvisio-maven.sh
+++ add-pathvisio-maven.sh
@@ -2,8 +2,26 @@
 
 ## Download PathVisio
 cd /tmp
-wget http://www.pathvisio.org/data/releases/3.1.3/pathvisio_bin-3.1.3-r3968.tar.gz
+wget -nc http://www.pathvisio.org/data/releases/3.1.3/pathvisio_bin-3.1.3-r3968.tar.gz
 tar -xvf pathvisio_bin-3.1.3-r3968.tar.gz pathvisio-3.1.3/pathvisio.jar --strip-components 1
+
+## Extract all jar files within pathvisio.jar and then add the extracted files
+mkdir pathvisio-jars
+unzip -q -o pathvisio.jar "*.jar" -d pathvisio-jars
+zip -d pathvisio.jar "*.jar"
+mkdir pathvisio-unjars
+find pathvisio-jars -name "*.jar" -exec unzip -q -o {} -d pathvisio-unjars  \;
+cd pathvisio-unjars
+rm -R META-INF
+zip -r ../pathvisio.jar *
+cd ..
+rm -R pathvisio-jars
+rm -R pathvisio-unjars
+
+## Remove all jar files within pathvisio.jar
+
+find . -name "*.jar" | while read filename; do unzip -o -d "`basename -s .zip "$filename"`" "$filename"; done;
+
 
 ## Install maven library
 mvn install:install-file -Dfile=pathvisio.jar -DgroupId=org.pathvisio -DartifactId=core -Dversion=3.1.3 -Dpackaging=jar
